c88da083ff67cae5b96a497337417ad1bb4409ec
Rushnett <rushnett@gmail.com>
2019-01-08 23:35:11 +0000

    variable/log cleanup

--- starboard.js
+++ starboard.js
@@ -5,19 +5,20 @@
  */
 
 // discord init
-var Discord = require("discord.js");
-var client = new Discord.Client();
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
 // is message posted object
-var messagePosted = {};
+let messagePosted = {};
 // emoji that goes in the post title
-var tt = ":star:";
+const tt = ":star:";
 
-// get settings
+let settings;
+let guildID = "";
+let smugboardID = "";
+
 try {
-  var settings = require("./settings.json");
-  var guildID = settings.serverID;
-  var smugboardID = settings.channelID;
+  settings = require("./settings.json");
 } catch (e) {
   console.log("a settings.json file has not been generated." + e.stack);
   process.exit();
@@ -25,27 +26,24 @@ try {
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
+  guildID = settings.serverID;
+  smugboardID = settings.channelID;
 });
 
 client.on('messageReactionAdd', (reaction, user) => {
   // if channel is posting channel
-  if (reaction.message.channel.id == smugboardID) {
-    return;
-  }
+  if (reaction.message.channel.id == smugboardID) return;
   //if message is from bot
-  if (reaction.message.author.username == client.user.username) {
-    return;
-  }
+  if (reaction.message.author.username == client.user.username) return;
   // if reaction is not desired emoji
-  if (reaction.emoji.name !== settings.reactionEmoji) {
-    return;
-  }
+  if (reaction.emoji.name !== settings.reactionEmoji) return;
 
   const msg = reaction.message;
   const msgID = msg.id;
   const msgChannelID = msg.channel.id;
   const channel = client.guilds.get(guildID).channels.get(smugboardID);
   const channelID = channel.id;
+  let eURL;
 
   console.log("message " + settings.reactionEmoji + "'d! (" + msgID + ")");
 
@@ -60,36 +58,35 @@ client.on('messageReactionAdd', (reaction, user) => {
   // did message reach threshold
   if (reaction.count >= settings.threshold) {
     messagePosted[msgID].latestCount = reaction.count;
-    console.log("posting message to channel " + smugboardID + ". reaction count: " + reaction.count);
     // if message is already posted
     if (messagePosted[msgID].hasOwnProperty("psm")) {
-      console.log("MESSAGE PREVIOUS EXIST");
-      var editableMessageID = messagePosted[msgID].psm;
+      let editableMessageID = messagePosted[msgID].psm;
+      console.log("updating count of message with ID " + editableMessageID + ". reaction count: " + reaction.count);
       channel.fetchMessage(editableMessageID).then(message => {
         message.edit(tt + '  **' + reaction.count + '** ' + '<#' + msgChannelID + '> (' + msgID + ')');
       });
     } else {
       // if message has already been created
-      if (messagePosted[msgID].posted) {
-        return;
-      }
+      if (messagePosted[msgID].posted) return;
+
+      console.log("posting message with content ID " + msgID + ". reaction count: " + reaction.count);
       // add message to ongoing object in memory
       messagePosted[msgID].posted = true;
       // header of star post =>
       channel.sendMessage(tt + '  ' + reaction.count + ' ' + '<#' + msgChannelID + '> (' + msgID + ')').then(message => {
         messagePosted[msgID].psm = message.id;
 
-        var embeds = msg.embeds;
-        var attachments = msg.attachments;
+        const embeds = msg.embeds;
+        const attachments = msg.attachments;
 
         // look for image imbeds from links
         if (embeds.length > 0) {
           // attempt to resolve image url; if none exist, ignore it
           try {
-            var eURL = embeds[0].thumbnail.url;
+            eURL = embeds[0].thumbnail.url;
           } catch (e) {
             console.log("error resolving thumbnail image. ignoring it.")
-            var eURL = embeds[0].url;
+            eURL = embeds[0].url;
           }
           console.log(eURL);
           const embed = new Discord.RichEmbed().setAuthor(msg.author.username, msg.author.avatarURL).setColor(0x00AE86).setDescription(msg.content).setImage(eURL).setTimestamp(new Date());
@@ -97,9 +94,9 @@ client.on('messageReactionAdd', (reaction, user) => {
 
           // look for image attachments
         } else if (attachments.array().length > 0) {
-          var attARR = attachments.array();
+          let attARR = attachments.array();
 
-          var eURL = attARR[0].url;
+          eURL = attARR[0].url;
 
           const embed = new Discord.RichEmbed().setAuthor(msg.author.username, msg.author.avatarURL).setColor(0x00AE86).setDescription(msg.content).setImage(eURL).setTimestamp(new Date());
           channel.sendEmbed(embed);
