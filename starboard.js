af5f8418dac98312ef66650ef83f5131f4677631
Nick <rushnett@gmail.com>
2019-12-20 14:35:39 -0600

    format code consistently

--- starboard.js
+++ starboard.js
@@ -4,38 +4,37 @@
  * Developed by Rushnett and Keiaxx.
  */
 
-
 // discord init
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 const client = new Discord.Client({
-  partials: Object.values(Discord.Constants.PartialTypes),
-});
+  partials: Object.values(Discord.Constants.PartialTypes)
+})
 
 // emoji that goes in the post title
-const tt = '⭐';
-let settings;
-let guildID = '';
-let smugboardID = '';
-let messagePosted = {};
+const tt = '⭐'
+let settings
+let guildID = ''
+let smugboardID = ''
+let messagePosted = {}
 
 try {
-  settings = require('./settings.json');
+  settings = require('./settings.json')
 } catch (e) {
-  console.log(`a settings.json file has not been generated. ${e.stack}`);
-  process.exit();
+  console.log(`a settings.json file has not been generated. ${e.stack}`)
+  process.exit()
 }
 
-function login() {
+function login () {
   if (settings.token) {
-    console.log('Logging in with token...');
-    client.login(settings.token);
+    console.log('Logging in with token...')
+    client.login(settings.token)
   } else {
-    console.log('Error logging in: There may be an issue with you settings.json file');
+    console.log('Error logging in: There may be an issue with you settings.json file')
   }
 }
 
-async function loadIntoMemory(client, guildID, channelID, limit) {
-  const channel = client.guilds.get(guildID).channels.get(channelID);
+async function loadIntoMemory (client, guildID, channelID, limit) {
+  const channel = client.guilds.get(guildID).channels.get(channelID)
   console.log(`Loading ${limit} messages...`)
 
   let messagesLeft = 0
@@ -51,27 +50,24 @@ async function loadIntoMemory(client, guildID, channelID, limit) {
     if (messagesLeft > 100) {
       messagesLeft = messagesLeft - 100
       console.log(`${messagesLeft} messages left to load`)
-      let moreMessages = await channel.messages.fetch({ limit: 100, before: messages.lastKey() })
+      const moreMessages = await channel.messages.fetch({ limit: 100, before: messages.lastKey() })
       messages = await messages.concat(moreMessages)
     } else {
       console.log(`${messagesLeft} messages left to load`)
-      let moreMessages = await channel.messages.fetch({ limit: messagesLeft, before: messages.lastKey() })
+      const moreMessages = await channel.messages.fetch({ limit: messagesLeft, before: messages.lastKey() })
       messages = await messages.concat(moreMessages)
       messagesLeft = 0
     }
   }
 
-  const posts = messages.filter(m => m.content.match(/\((\d{18})\)/));
+  const posts = messages.filter(m => m.content.match(/\((\d{18})\)/))
   const newPosts = messages.filter(m => {
-    if(m.embeds.length > 0){
-      if(m.embeds[0].footer)
-        return String(m.embeds[0].footer.text).match(/\((\d{18})\)/)
-      else
-        return false
-    }else{
+    if (m.embeds.length > 0) {
+      if (m.embeds[0].footer) { return String(m.embeds[0].footer.text).match(/\((\d{18})\)/) } else { return false }
+    } else {
       return false
     }
-  });
+  })
 
   const postsMap = posts.reduce((mapAccumulator, obj) => {
     // either one of the following syntax works
@@ -80,12 +76,11 @@ async function loadIntoMemory(client, guildID, channelID, limit) {
       p: true,
       lc: settings.threshold + 1,
       legacy: true,
-      psm: obj.id,
-    };
+      psm: obj.id
+    }
 
-    return mapAccumulator;
-  }, {});
-
+    return mapAccumulator
+  }, {})
 
   const newPostsMap = newPosts.reduce((mapAccumulator, obj) => {
     // either one of the following syntax works
@@ -94,42 +89,39 @@ async function loadIntoMemory(client, guildID, channelID, limit) {
       p: true,
       lc: settings.threshold + 1,
       legacy: false,
-      psm: obj.id,
-    };
+      psm: obj.id
+    }
 
-    return mapAccumulator;
-  }, {});
+    return mapAccumulator
+  }, {})
 
-  messagePosted = {...postsMap, ...newPostsMap};
+  messagePosted = { ...postsMap, ...newPostsMap }
 
-  console.log(`Loaded ${Object.keys(postsMap).length} legacy posts, and ${Object.keys(newPostsMap).length} new posts in ${settings.reactionEmoji} channel`);
+  console.log(`Loaded ${Object.keys(postsMap).length} legacy posts, and ${Object.keys(newPostsMap).length} new posts in ${settings.reactionEmoji} channel`)
 
-  console.log(`Loading complete`)
+  console.log('Loading complete')
 }
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.username}!`);
-  guildID = settings.serverID;
-  smugboardID = settings.channelID;
+  console.log(`Logged in as ${client.user.username}!`)
+  guildID = settings.serverID
+  smugboardID = settings.channelID
   // fetch existing posts
-  loadIntoMemory(client, guildID, smugboardID, settings.fetchLimit);
-});
+  loadIntoMemory(client, guildID, smugboardID, settings.fetchLimit)
+})
 
 client.on('messageReactionAdd', (reaction_orig, user) => {
   // if channel is posting channel
-  if (reaction_orig.message.channel.id == smugboardID) return;
+  if (reaction_orig.message.channel.id == smugboardID) return
   // if reaction is not desired emoji
-  if (reaction_orig.emoji.name !== settings.reactionEmoji) return;
+  if (reaction_orig.emoji.name !== settings.reactionEmoji) return
 
-
-  const msg = reaction_orig.message;
-  const msgID = msg.id;
-  const msgChannelID = msg.channel.id;
-  const msgChannel = client.guilds.get(guildID).channels.get(msgChannelID);
-  const msgLink = `https://discordapp.com/channels/${guildID}/${msgChannelID}/${msgID}`;
-  const channel = client.guilds.get(guildID).channels.get(smugboardID);
-
-
+  const msg = reaction_orig.message
+  const msgID = msg.id
+  const msgChannelID = msg.channel.id
+  const msgChannel = client.guilds.get(guildID).channels.get(msgChannelID)
+  const msgLink = `https://discordapp.com/channels/${guildID}/${msgChannelID}/${msgID}`
+  const channel = client.guilds.get(guildID).channels.get(smugboardID)
 
   // if message doesnt exist yet in memory, create it
   if (!messagePosted.hasOwnProperty(msgID)) {
@@ -138,75 +130,62 @@ client.on('messageReactionAdd', (reaction_orig, user) => {
     messagePosted[msgID] = {
       p: false,
       lc: 0
-    };
-  }else{
-    if(messagePosted[msgID].legacy){
+    }
+  } else {
+    if (messagePosted[msgID].legacy) {
       console.log(`Legacy message ${settings.reactionEmoji}'d, ignoring`)
-      return;
+      return
     }
   }
 
   msgChannel.messages.fetch(msg.id).then((msg) => {
-
     // if message is older than set amount
-    let dateDiff = (new Date()) - reaction_orig.message.createdAt;
-    let dateCutoff = 1000 * 60 * 60 * 24;
+    const dateDiff = (new Date()) - reaction_orig.message.createdAt
+    const dateCutoff = 1000 * 60 * 60 * 24
     if (Math.floor(dateDiff / dateCutoff) >= settings.dateCutoff) {
-      console.log(`a message older than ${settings.dateCutoff} days was reacted to, ignoring`);
-      return;
+      console.log(`a message older than ${settings.dateCutoff} days was reacted to, ignoring`)
+      return
     }
 
-    //We need to do this because the reaction count seems to be 1 if an old cached
-    //message is starred. This is to get the 'actual' count
+    // We need to do this because the reaction count seems to be 1 if an old cached
+    // message is starred. This is to get the 'actual' count
     msg.reactions.forEach((reaction) => {
-
       if (reaction.emoji.name == settings.reactionEmoji) {
-        console.log(`message ${settings.reactionEmoji}'d! (${msgID}) in #${msgChannel.name} total: ${reaction.count}`);
+        console.log(`message ${settings.reactionEmoji}'d! (${msgID}) in #${msgChannel.name} total: ${reaction.count}`)
         // did message reach threshold
         if (reaction.count >= settings.threshold) {
-          messagePosted[msgID].lc = reaction.count;
+          messagePosted[msgID].lc = reaction.count
           // if message is already posted
           if (messagePosted[msgID].hasOwnProperty('psm')) {
-
-            const editableMessageID = messagePosted[msgID].psm;
-            console.log(`updating count of message with ID ${editableMessageID}. reaction count: ${reaction.count}`);
+            const editableMessageID = messagePosted[msgID].psm
+            console.log(`updating count of message with ID ${editableMessageID}. reaction count: ${reaction.count}`)
             const messageFooter = `${reaction.count} ${tt} (${msgID})`
             channel.messages.fetch(editableMessageID).then((message) => {
               message.embeds[0].setFooter(messageFooter)
               message.edit(message.embeds[0])
-            });
-
+            })
           } else {
             // if message has already been created
-            if (messagePosted[msgID].p) return;
+            if (messagePosted[msgID].p) return
 
-            console.log(`posting message with content ID ${msgID}. reaction count: ${reaction.count}`);
+            console.log(`posting message with content ID ${msgID}. reaction count: ${reaction.count}`)
             // add message to ongoing object in memory
-            messagePosted[msgID].p = true;
+            messagePosted[msgID].p = true
 
             // create content message
-            const contentMsg = `${msg.content}\n\n→ [original message](${msgLink}) in <#${msgChannelID}>`;
-            const avatarURL = `https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}.jpg`;
-            const embeds = msg.embeds;
-            const attachments = msg.attachments;
+            const contentMsg = `${msg.content}\n\n→ [original message](${msgLink}) in <#${msgChannelID}>`
+            const avatarURL = `https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}.jpg`
+            const embeds = msg.embeds
+            const attachments = msg.attachments
             const messageFooter = `${reaction.count} ${tt} (${msgID})`
             let eURL = ''
 
-
-
             if (embeds.length > 0) {
-
               // attempt to resolve image url; if none exist, ignore it
-              if(embeds[0].thumbnail && embeds[0].thumbnail.url)
-                eURL = embeds[0].thumbnail.url;
-              else if(embeds[0].image && embeds[0].image.url)
-                eURL = embeds[0].image.url;
-              else
-                eURL = embeds[0].url;
-
+              if (embeds[0].thumbnail && embeds[0].thumbnail.url) { eURL = embeds[0].thumbnail.url } else if (embeds[0].image && embeds[0].image.url) { eURL = embeds[0].image.url } else { eURL = embeds[0].url }
             } else if (attachments.array().length > 0) {
-              const attARR = attachments.array();
-              eURL = attARR[0].url;
+              const attARR = attachments.array()
+              eURL = attARR[0].url
               // no attachments or embeds
             }
 
@@ -216,17 +195,17 @@ client.on('messageReactionAdd', (reaction_orig, user) => {
               .setDescription(contentMsg)
               .setImage(eURL)
               .setTimestamp(new Date())
-              .setFooter(messageFooter);
+              .setFooter(messageFooter)
             channel.send({
               embed
             }).then((starMessage) => {
-              messagePosted[msgID].psm = starMessage.id;
-            });
+              messagePosted[msgID].psm = starMessage.id
+            })
           }
         }
       }
-    });
-  });
-});
+    })
+  })
+})
 
-login();
+login()
